3cbfa7695c08bfd544af048f5db10c81634255de
Abdoulkarim <17a.karim@gmail.com>
2025-06-19 22:17:37 +0100

    feat(cards): implement flip toggle on navigation button click

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const cardContainer = document.querySelector(".cards-wrapper");
 const logo = document.querySelector(".logo h1");
 const logoText = logo.textContent;
 logo.textContent = "";
@@ -9,4 +10,14 @@ logo.textContent = "";
   span.textContent = char;
   span.style.animationDelay = `${i * 50}ms`;
   logo.append(span);
+});
+
+//? Even listener for toggling front/back cards
+
+cardContainer.addEventListener("click", (e) => {
+  const toggleCardBtn = e.target;
+  const card = e.target.closest(".card");
+  if (!toggleCardBtn.classList.contains("navigation-button")) return;
+
+  card.classList.toggle("change");
 });
